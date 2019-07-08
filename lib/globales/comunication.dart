5b2b319a69d95e9b17aaeada6695e17b5e283dfd
Jimmy Vicente <jimmy.vicente@unl.edu.ec>
2019-07-08 10:51:23 -0500

    borrar archivos mal subidos

--- lib/globales/comunication.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Link{
-  static final String IP_CONEXION= "http://178.62.18.201:9090";
-  static final String IP_RECURSO = "http://testktaxi.kradac.com";
-}
